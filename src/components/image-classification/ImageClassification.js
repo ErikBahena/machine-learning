1eaee80ce7fa3b7f1f53da1b9b05157ecc26a7ef
ErikB <80177106+ErikBahena@users.noreply.github.com>
2022-04-30 13:32:20 -0700

    added ability to choose file

--- src/components/image-classification/ImageClassification.js
+++ src/components/image-classification/ImageClassification.js
@@ -20,10 +20,31 @@ export default function ImageClassification() {
     initializeModel()
   }, [])
 
-  const classifyImage = async () => {
-    const res = await mobileNet.classify(imageEl.current, 5)
+  const reader = new FileReader()
 
-    console.log(res, "HERE")
+  function handleEvent(event) {
+    if (event.type === "load") {
+      imageEl.current.src = reader.result
+
+      classifyImage(imageEl.current)
+    }
+  }
+
+  function addListeners(reader) {
+    reader.addEventListener("load", handleEvent)
+  }
+
+  function handleSelected(e) {
+    const selectedFile = e.target.files[0]
+    if (selectedFile) {
+      addListeners(reader)
+      reader.readAsDataURL(selectedFile)
+    }
+  }
+
+  const classifyImage = async (image) => {
+    const res = await mobileNet.classify(image, 4)
+
     setPredictions(res)
   }
 
@@ -32,28 +53,17 @@ export default function ImageClassification() {
       <Link to="/">Go Back</Link>
       <h1>Image classification using MobileNet from Ml5</h1>
 
-      <img
-        src="images/king-penguin.jpeg"
-        ref={imageEl}
-        crossOrigin="anonymous"
-      />
+      <img src={null} ref={imageEl} alt="" />
 
-      <button onClick={() => classifyImage()}>Classify</button>
-
-      {/* <label htmlFor="image-to-classify">Choose an Image:</label> */}
-      {/* <input
+      <label htmlFor="image-to-classify">Choose an Image:</label>
+      <input
         type="file"
         id="image-to-classify"
-        name="image-to-classify"
-        accept="image/png, image/jpeg"
-        onChange={(e) => {
-          imageEl.current.src = URL.createObjectURL(e.target.files[0])
+        accept="image/*"
+        onChange={handleSelected}
+      />
 
-          classifyImage()
-        }}
-      /> */}
-
-      {predictions.length &&
+      {predictions.length > 0 &&
         predictions.map((prediction, i) => {
           return (
             <p key={i}>
